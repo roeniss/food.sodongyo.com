dee2a5da8896ab03d290d050d2497c588a7ad560
RoenissMoon <roeniss2@gmail.com>
2020-03-18 15:51:48 +0900

    헤더 디자인 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,21 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 const Header = () => {
   return (
-    <Navbar expand="xl" bg="dark" variant="dark" fixed="top">
-      <Navbar.Brand href="/">맛게살</Navbar.Brand>
+    <Navbar expand="xl" bg="primary" variant="dark" fixed="top">
+      <Navbar.Brand href="/" class="text-white">
+        맛동요 <span className="hide-on-overflow">:: 맛게살의 동선 요기있네 </span> 🍕🍗🍺
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto"></Nav>
+        <Nav>
+          <Nav.Link href="#">필터링</Nav.Link>
+          <Nav.Link href="#">리뷰 등록하기</Nav.Link>
+          <Nav.Link href="#">개발자에게 메시지 보내기</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
