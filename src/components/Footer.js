22acb2d2cecb72fb5e219b8092fc9a629c7c6932
RoenissMoon <roeniss2@gmail.com>
2020-03-18 15:49:29 +0900

    푸터 디자인, 메시지, disclaimer 추가, 변경

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,18 +5,17 @@ import MarkerController from "../lib/MarkerController";
 const Footer = () => {
   return (
     <>
-      <style type="text/css">
-        {`
-        .navbar-footer {
-          padding: 10px 5px;
-        }
-        `}
-      </style>
-
-      <Navbar fixed="bottom" bg="light" variant="footer" className="justify-content-between">
-        <Button variant="primary">총 {MarkerController.locations.length}개</Button>
-        <Button variant="secondary">Secondary</Button>
-        <Button variant="success">Success</Button>
+      <Navbar fixed="bottom" bg="secondary" variant="footer" className="justify-content-center">
+        {/* <Button variant="primary">총 {MarkerController.locations.length}개</Button> */}
+        {/* <Button variant="secondary">Secondary</Button> */}
+        {/* <Button variant="success">Success</Button> */}
+        <span className="disclaimer">
+          <i>
+            본 사이트의 모든 리뷰는 별도의 혜택을 제공받지 않았으며, 맛게살은 비영리 프로젝트입니다.
+          </i>
+          <br />
+          맛동요 :: All Rights Reserved.
+        </span>
       </Navbar>
     </>
   );
